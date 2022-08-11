05090a79d4159f4541d770268722c5c9af5e4915
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-11 22:12:10 +0530

    added: calculator addition feature

---
+++ src/javasample/calculator.java
@@ -0,0 +1,13 @@
+package javasample;
+
+public class calculator {
+	public static void main(String[] args) {
+		calculator calculator = new calculator();
+		int addition = calculator.add(10, 20);
+		System.out.println("sum of two number is; " + addition);
+	}
+
+	public int add(int number1, int number2) {
+		return number1 + number2;
+	}
+}
